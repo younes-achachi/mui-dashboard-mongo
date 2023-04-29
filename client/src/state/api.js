6380f192606f119630c03c6528c7b6ce085b63d5
younes achachi <younes.achachi1@gmail.com>
2023-04-29 05:30:27 +0100

    add  process env to stat api base query

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const api = createApi({
 	baseQuery: fetchBaseQuery({
-		baseUrl: 'http://localhost:5001'
+		baseUrl: process.env.REACT_APP_BASE_URL
 	}),
 
 	reducerPath: 'adminApi',
